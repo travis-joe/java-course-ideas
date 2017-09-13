96461f70e26017c69dc4afcb799df18abbf31961
qzy <weelgod@gmail.com>
2017-09-13 08:11:48 +0800

    save

---
+++ src/main/java/com/travisJoe/courses/Main.java
@@ -0,0 +1,35 @@
+package com.travisJoe.courses;
+
+import com.travisJoe.courses.model.CourseIdeaDAO;
+import com.travisJoe.courses.model.SimpleCourseIdeaDAO;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in",(req,res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username",username);
+            model.put("username", username);
+            return new ModelAndView(model,"sign-in.hbs");
+        },new HandlebarsTemplateEngine());
+    }
+}
---
+++ src/main/java/com/travisJoe/courses/model/CourseIdeaDAO.java
@@ -0,0 +1,10 @@
+package com.travisJoe.courses.model;
+
+import java.util.List;
+
+public interface CourseIdeaDAO {
+    boolean add(CourseIdea idea);
+
+    List<CourseIdea> findAll();
+
+}
